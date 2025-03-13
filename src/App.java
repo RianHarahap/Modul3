1c4637fec7f171c505f09c55f0e10ed6ceb89393
RianHarahap <rianharahap2303@gmail.com>
2025-03-13 16:47:51 +0700

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,136 +1,52 @@
 import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception 
-    {
-        System.out.println("///Percobaan Pertama///");
-        boolean x = false;
-        if(!x){
-            System.out.println("Ini adalah blok if");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan kalimat: ");
+        String kalimat = input.nextLine();
+        
+        int jumlahVokal = 0;
+        
+        for (int i = 0; i < kalimat.length(); i++) {
+            char huruf = kalimat.charAt(i);
+            
+            if (huruf == 'a' || huruf == 'A') {
+                jumlahVokal++;
+            } else if (huruf == 'i' || huruf == 'I') {
+                jumlahVokal++;
+            } else if (huruf == 'u' || huruf == 'U') {
+                jumlahVokal++;
+            } else if (huruf == 'e' || huruf == 'E') {
+                jumlahVokal++;
+            } else if (huruf == 'o' || huruf == 'O') {
+                jumlahVokal++;
+            }
         }
-         else {
-            System.out.println("Ini adalah blok else");
-        }
+        
+        System.out.println("Jumlah huruf vokal: " + jumlahVokal);
+        
 
-
-        System.out.println("///Percobaan Kedua///");
-
-        int a = 10;
-        if (a > 5)
-        {
-            System.out.println("A lebih besar dari 5");
-        }
-        else 
-        {
-            System.out.println("A lebih kecil dari 5");
-        }
-
-
-        System.out.println("///Percobaan Ketiga///");
-        Scanner input = new Scanner (System.in);
-        System.out.println("Masukkan Angka: " );
-        int nilai = input.nextInt();
-
-        if(nilai > 80)
-        {
-            System.out.println("Nilai Anda A");
-        }
-        else if (nilai > 70) 
-        {
-            System.out.println("Nilai Anda B");
-        }
-        else if (nilai > 60)
-        {
-            System.out.println("Nilai Anda C");
-        }
-        else if (nilai > 50)
-        {
-            System.out.println("Nilai Anda D");
-        }
-        else {
-            System.out.println("Nilai Anda E");
-        }
-                input.close();
-
-
-        System.out.println("///Percobaan Keempat///");
-        int day = 1;
-        switch (day)
-        {
+        switch (jumlahVokal % 2) {
+            case 0:
+                System.out.println("Jumlah huruf vokal genap");
+                break;
             case 1:
-                System.out.println("Senin");
-                break;
-            
-            case 2:
-                System.out.println("Selasa");
-                break;
-
-            default:
-                System.out.println("Tidak Valid");
+                System.out.println("Jumlah huruf vokal adalah ganjil");
                 break;
         }
-
-
-        System.out.println("///Percobaan Kelima///");
-        String dayName = "Senin";
-        switch(dayName)
-        {
-            case "Senin":
-            System.out.println("Benar hari senin");
-            break;
-            
-            default:
-            System.out.println("Tidak Valid");
+        
+        System.out.print("String secara terbalik: ");
+        
+        for (int i = kalimat.length() - 1; i >= 0; i--) {
+            System.out.print(kalimat.charAt(i) + " ");
         }
+        
+        input.close();
 
 
-        System.out.println("///Percobaan Keenam///");
-        int angka = 4;
-        if (angka % 2 ==0)
-        {
-            System.out.println("Angka Genap");
-        }
-        else
-        {
-            System.out.println("Angka Ganjil");
-        }
-
-        System.out.println("///Percobaan Ketujuh");
-        String hasil = (angka % 2 == 0) ? "Angka Genap" : "Angka Ganjil";
-        System.out.println(hasil);
-
-
-        System.out.println("///Percobaan Kedelapan");
-        for(int i = 0; i < 5; i++)
-        {
-            System.out.println("Perulangan Ke-" + (i+1));
-        }
-
-
-        System.out.println("///Percobaal Kesembilan");
-        int i = 0;
-        while (i < 5)
-        {
-            System.out.println("Perulangan Ke" + (i+1));
-            i++;
-        }
-
-
-        System.err.println("///Percobaan Kesepuluh///");
-        do
-        {
-            System.out.println("Perulangan Ke- " + (i+1));
-            i++;
-        }
-        while (i < 5);
-
-
-        System.out.println("///Percobaan Kesebelas");
-        int []numbers ={1, 2, 3, 4, 5};
-        for (int j : numbers)
-        {
-            System.out.println(j);
-        }
     }
 }
-       
+
 
